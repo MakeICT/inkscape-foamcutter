d844615795c85bb9ab30395b105fd4048d6f4076
Dominic Canare <dom@makeict.org>
2015-10-08 19:58:46 -0500

    update defaults for foam-cutter

--- arduino/src/FoamCutterDriver.ino
+++ arduino/src/FoamCutterDriver.ino
@@ -27,14 +27,14 @@
  * PINS
  */
  
+#define XAXIS_ENABLE_PIN 6
 #define XAXIS_DIR_PIN 7
 #define XAXIS_STEP_PIN 8
-#define XAXIS_ENABLE_PIN 6
 #define XAXIS_ENDSTOP_PIN 3
 
+#define YAXIS_ENABLE_PIN 9
 #define YAXIS_DIR_PIN 10
 #define YAXIS_STEP_PIN 11
-#define YAXIS_ENABLE_PIN 9
 #define YAXIS_ENDSTOP_PIN -1 // <0 0> No Endstop!
 
 #define SERVO_PIN 2
@@ -43,7 +43,7 @@
  * Other Configuration
  */
 
-#define DEFAULT_PEN_UP_POSITION 170
+#define DEFAULT_PEN_UP_POSITION 180
 #define XAXIS_MIN_STEPCOUNT -467
 #define XAXIS_MAX_STEPCOUNT 467
 #define DEFAULT_ZOOM_FACTOR 1. // With a Zoom-Faktor of .65, I can print gcode for Makerbot Unicorn without changes. 
